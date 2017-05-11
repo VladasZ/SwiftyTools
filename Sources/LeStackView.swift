edbe7aec72d63ed821875909641be1ef308a2f7c
Vladas Zakrevskis <146100@gmail.com>
2017-05-11 21:31:00 +0300

    added LeStackView

---
+++ Sources/LeStackView.swift
@@ -0,0 +1,70 @@
+//
+//  LeStackView.swift
+//  swiftSand
+//
+//  Created by Vladas Zakrevskis on 4/20/17.
+//  Copyright © 2017 VladasZ. All rights reserved.
+//
+
+import UIKit
+
+public class LeStackView : UIView {
+    
+    //MARK: - Properties
+    
+    var isHorizontal = true
+    
+    var arrangedSubviews: [UIView]! {
+        didSet {
+            guard let subviews = arrangedSubviews else { print("❤️"); return }
+            setupArrangedSubviews(subviews)
+        }
+    }
+        
+    private var spacing: CGFloat {
+        
+        return (isHorizontal ? frame.size.width : frame.size.height) / CGFloat(arrangedSubviews.count)
+    }
+    
+    //MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setup()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        setup()
+    }
+    
+    //MARK: - Internal Initialization
+    
+    private func setup () {
+        
+    }
+    
+    //MARK: - UI Setup
+    
+    private func centerPositionForViewAtIndex(_ index: Int) -> CGPoint {
+        
+        var x = spacing * CGFloat(index) + spacing / 2
+        var y = (isHorizontal ? frame.size.height : frame.size.width) / 2
+        
+        if !isHorizontal { swap(&x, &y) }
+        
+        return CGPoint(x: x, y: y)
+    }
+    
+    private func setupArrangedSubviews(_ subviews: [UIView]) {
+        
+        for i in 0...subviews.count - 1 {
+            
+            addSubview(subviews[i])
+            subviews[i].center = centerPositionForViewAtIndex(i)
+        }
+    }
+}
+
--- SwiftTools.podspec
+++ SwiftTools.podspec
@@ -1,6 +1,7 @@
 Pod::Spec.new do |s|
 s.name           = 'SwiftTools'
-s.version        = '0.1.4'
+s.version        = '0.1.5
+'
 s.summary        = "Swift tools kit to make your life easier."
 s.homepage       = "https://github.com/VladasZ/SwiftTools"
 s.author         = { 'Vladas Zakrevskis' => '146100@gmail.com' }
