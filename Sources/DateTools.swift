3e6d9d5e12cbe4ed3816eb768407f21e6627675e
Vladas Zakrevskis <146100@gmail.com>
2017-06-08 17:30:04 +0300

    Date time components  properties

--- Sources/DateTools.swift
+++ Sources/DateTools.swift
@@ -32,9 +32,12 @@ public extension Date {
     
     //MARK: - Elements
     
-    var day:   Int { return Calendar.current.component(.day,   from: self) }
-    var month: Int { return Calendar.current.component(.month, from: self) }
-    var year:  Int { return Calendar.current.component(.year,  from: self) }
+    var second: Int { return Calendar.current.component(.second, from: self) }
+    var minute: Int { return Calendar.current.component(.minute, from: self) }
+    var hour:   Int { return Calendar.current.component(.hour,   from: self) }
+    var day:    Int { return Calendar.current.component(.day,    from: self) }
+    var month:  Int { return Calendar.current.component(.month,  from: self) }
+    var year:   Int { return Calendar.current.component(.year,   from: self) }
     
     var monthString: String { return DateFormatter("MMMM").string(from: self) }
     
