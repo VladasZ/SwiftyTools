819d4d48718dcfd41804d84d9762efab71d2346b
Vladas Zakrevskis <146100@gmail.com>
2017-06-24 22:52:34 +0300

    TimersKit

---
+++ Sources/TimersKit.swift
@@ -0,0 +1,29 @@
+//
+//  TimersKit.swift
+//  SwiftTools
+//
+//  Created by Vladas Zakrevskis on 6/23/17.
+//  Copyright © 2017 VladasZ. All rights reserved.
+//
+
+import Foundation
+
+public typealias TimerAction = (inout Bool) -> ()
+
+public extension Timer {
+    
+    static func every(_ interval: Double, _ action: @escaping TimerAction) {
+        
+        let timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(self.timerAction(_:)), userInfo: action, repeats: true)
+        timer.fire()
+    }
+    
+    @objc private static func timerAction(_ timer: Timer) {
+ 
+        guard let action = timer.userInfo as? TimerAction else { Log.error(); timer.invalidate(); return }
+        
+        var stop: Bool = false
+        action(&stop)
+        if stop { timer.invalidate() }
+    }
+}
