30cdfd5c78949a20e6510c215283b5aa131b907d
Vladas Zakrevskis <146100@gmail.com>
2017-07-14 11:55:00 +0300

    added call counter

---
+++ Sources/Debug.swift
@@ -0,0 +1,29 @@
+//
+//  Debug.swift
+//  SwiftTools
+//
+//  Created by Vladas Zakrevskis on 7/14/17.
+//  Copyright © 2017 VladasZ. All rights reserved.
+//
+
+import Foundation
+
+class Debug {
+    
+    static var callCouterData = [String : Int]()
+    
+    static func callCounter(_ file: String = #file, _ function:String = #function, _ line: Int = #line) {
+        
+        let id = "\(file)\(line)"
+        
+        if let data = callCouterData[id] {
+            
+            Log.info("call count: \(data)", file, function, line)
+            callCouterData[id]! += 1
+        }
+        else {
+            Log.info("call count: \(1)", file, function, line)
+            callCouterData[id] = 1
+        }
+    }
+}
