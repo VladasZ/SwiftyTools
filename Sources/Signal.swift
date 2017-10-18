6540d35a0c6a6377770fac73285dc4fb9ec6c046
Vladas Zakrevskis <146100@gmail.com>
2017-10-18 12:14:54 +0300

    Object based Signal binding

--- Sources/Signal.swift
+++ Sources/Signal.swift
@@ -9,18 +9,43 @@
 import Foundation
 
 fileprivate struct SignalSubscriber<T> : Hashable {
-
+    
     static func ==(lhs: SignalSubscriber, rhs: SignalSubscriber) -> Bool {
         return lhs.hashValue == rhs.hashValue
     }
-
-    var hashValue: Int { return file.hashValue + line.hashValue }
     
-    var identifier: String { return file + String(line) }
+    var hashValue: Int {
+        
+        var hash = file.hashValue + line.hashValue
+        
+        if let hashObject = linkedObject?.hash {
+            hash += hashObject
+        }
+        
+        return hash
+    }
+    
+    var identifier: String {
+        
+        var id = file + String(line)
+        if let hash = linkedObject?.hash {
+            id.append(String(hash))
+        }
+        
+        return id
+    }
     
     var file: String
     var line: Int
     var action: (T) -> ()
+    weak var linkedObject: AnyObject?
+    
+    
+    init(file: String, line: Int, action: @escaping (T) -> ()) {
+        self.file = file
+        self.line = line
+        self.action = action
+    }
 }
 
 public class Signal<T> {
@@ -43,9 +68,23 @@ public class Signal<T> {
         subscribers[subscriber.identifier] = subscriber
     }
     
+    public func subscribe(_ file: String = #file, _ line: Int = #line, object: AnyObject, _ action: @escaping (T) -> ()) {
+        
+        var subscriber = SignalSubscriber(file: file, line: line, action: action)
+        subscriber.linkedObject = object
+        subscribers[subscriber.identifier] = subscriber
+    }
+    
     public func unsubscribeFile(_ file: String = #file) {
         
         let filterredArray = subscribers.filter { $0.value.file != file }
+        subscribers.removeAll()
+        filterredArray.forEach { subscribers[$0.key] = $0.value }
+    }
+    
+    public func unsubscribeObject() {
+        
+        let filterredArray = subscribers.filter { $0.value.linkedObject != nil }
         subscribers.removeAll()
         filterredArray.forEach { subscribers[$0.key] = $0.value }
     }
@@ -65,3 +104,4 @@ public class Signal<T> {
         else { onMain { _fire() } }
     }
 }
+
