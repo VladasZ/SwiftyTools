8772d49aa3272c6393f297271a2051e9f8348c81
Vladas Zakrevskis <146100@gmail.com>
2017-11-03 15:11:10 +0300

    Number class Bool type support

--- Sources/Number.swift
+++ Sources/Number.swift
@@ -16,6 +16,7 @@ public class Number {
     
     public func getValue<T>() -> T {
         switch T.self {
+        case is Bool.Type: return (value != "0") as! T
         case is String.Type: return value as! T
         case is Int.Type: return Int(value.withoutFractionPart) as! T
         case is Float.Type: return Float(value) as! T
@@ -26,11 +27,20 @@ public class Number {
     
     public init(_ value: Any?) throws { guard let value = value else { throw "Failed Number" }
         switch value {
-        case is String: self.value =        value as! String
+        case is String: let string = value as! String
+        if string == "true" || string == "false" {
+            self.value = string == "true" ? "1" : "0"
+        }
+        else {
+            guard Double(string) != nil else { throw "Invalid string value" }
+            self.value = string
+            }
         case is Int:    self.value = String(value as! Int)
         case is Float:  self.value = String(value as! Float)
         case is Double: self.value = String(value as! Double)
-        default: throw "Failed Number"
+        case is Bool:   self.value = (value as! Bool) ? "1" : "0"
+        default: throw "Unsupported class"
         }
     }
 }
+
