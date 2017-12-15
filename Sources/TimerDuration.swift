db6870bd9d23604a1cf8491ed6cea8d7aea3c78f
Vladas Zakrevskis <146100@gmail.com>
2017-12-15 10:41:55 +0300

    TimerDuration class

---
+++ Sources/TimerDuration.swift
@@ -0,0 +1,98 @@
+//
+//  TimerDuration.swift
+//  SwiftyTools
+//
+//  Created by Vladas Zakrevskis on 3/2/17.
+//  Copyright © 2017 Vladas Zakrevskis. All rights reserved.
+//
+
+import Foundation
+
+fileprivate let secondsInHour   = 60 * 60
+fileprivate let secondsInMinute = 60
+
+class TimerDuration {
+    
+    private var startDate: TimeInterval = 0
+    private var pauseDate: TimeInterval = 0
+    private var stopDate:  TimeInterval = 0
+    
+    var totalSeconds: TimeInterval {
+        get {
+            if pauseDate == 0 && stopDate == 0 {
+                return Date().timeIntervalSinceReferenceDate - startDate
+            }
+            else if stopDate == 0 {
+                return pauseDate - startDate
+            }
+            else {
+                return stopDate - startDate
+            }
+        }
+        set {
+            startDate = 0
+            stopDate = newValue
+        }
+    }
+    
+    var isPaused: Bool = false {
+        didSet {
+            
+            if isPaused {
+                
+                pauseDate = Date().timeIntervalSinceReferenceDate
+            }
+            else {
+                
+                guard pauseDate != 0 else { return }
+                
+                startDate += Date.timeIntervalSinceReferenceDate - pauseDate
+                pauseDate = 0
+            }
+        }
+    }
+    
+    var stopped: Bool { return stopDate != 0 }
+    
+    //MARK: - Interface
+    
+    func start() {
+        
+        startDate = Date().timeIntervalSinceReferenceDate
+    }
+
+    func stop() {
+        
+        isPaused = false
+        stopDate = Date().timeIntervalSinceReferenceDate
+    }
+    
+    var String: String {
+        
+        var seconds = Int(self.totalSeconds)
+        
+        let hours = seconds / secondsInHour
+        seconds %= secondsInHour
+        let minutes = seconds / secondsInMinute
+        seconds %= secondsInMinute
+        
+        return "\(timeComponentToString(hours)):\(timeComponentToString(minutes)):\(timeComponentToString(seconds))"
+    }
+    
+    var description: String {
+        
+        var seconds = Int(self.totalSeconds)
+        
+        seconds %= secondsInHour
+        let minutes = seconds / secondsInMinute
+        seconds %= secondsInMinute
+        
+        return "\(timeComponentToString(minutes)):\(timeComponentToString(seconds))"
+    }
+    
+    private func timeComponentToString(_ component: Int) -> String {
+        
+        if component < 10 { return "0\(component)"}
+        return  "\(component)"
+    }
+}
