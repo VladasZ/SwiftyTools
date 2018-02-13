f38bd94be032121cc54c332b729e26ae1a4c59ad
Vladas Zakrevskis <146100@gmail.com>
2018-02-13 17:05:55 +0300

    Removed obsolete methods and properties from Date extensions

--- Sources/Extensions/DateTools.swift
+++ Sources/Extensions/DateTools.swift
@@ -8,10 +8,6 @@
 
 import Foundation
 
-fileprivate let dateTimeFormatter    = DateFormatter("HH:mm dd-MM-yyyy")
-fileprivate let dateFormatter        = DateFormatter("dd-MM-yyyy")
-fileprivate let hourMinTimeFormatter = DateFormatter("HH:mm")
-fileprivate let minSecTimeFormatter  = DateFormatter("mm:ss")
 fileprivate var dateFormatterLocale: Locale?
 
 public extension DateFormatter {
@@ -19,10 +15,8 @@ public extension DateFormatter {
     public static func setLocale(_ locale: Locale) { dateFormatterLocale = locale }
 
     public convenience init(_ format: String) {
-        
         self.init()
         self.dateFormat = format
-        
         if let locale = dateFormatterLocale { self.locale = locale }
     }
 }
@@ -38,27 +32,20 @@ public extension Date {
     var month:  Int { return Calendar.current.component(.month,  from: self) }
     var year:   Int { return Calendar.current.component(.year,   from: self) }
     
-    var monthString: String { return DateFormatter("MMMM").string(from: self) }
-    
     //MARK: - Initializatiors
     
     public static func with(year: Int, month: Int, day: Int) -> Date {
-        
         let gregorianCalendar = NSCalendar(calendarIdentifier: .gregorian)!
-        
         var dateComponents = DateComponents()
         dateComponents.year = year
         dateComponents.month = month
         dateComponents.day = day
-        
         let date = gregorianCalendar.date(from: dateComponents)!
         return date
     }
     
     public static func todayWith(hour: Int, minute: Int, second: Int) -> Date {
-        
         let current = Date()
-        
         var dateComponents = DateComponents()
         dateComponents.year = current.year
         dateComponents.month = current.month
@@ -66,24 +53,14 @@ public extension Date {
         dateComponents.hour = hour
         dateComponents.minute = minute
         dateComponents.second = second
-        
         return NSCalendar(calendarIdentifier: .gregorian)!.date(from: dateComponents)!
     }
     
-    public static func parse(_ dateString: String, format: String? = nil) -> Date {
-        
-        if let format = format {
-            let formatter = DateFormatter(format)
-            if let date = formatter.date(from: dateString) { return date }
-            else { Log.error(); return Date() }
-        }
-        
-        if let date = dateTimeFormatter.date(from: dateString) { return date }
-        else { Log.error(); return Date() }
+    public static func parse(_ dateString: String, format: String) -> Date? {
+        return DateFormatter(format).date(from: dateString)
     }
     
     public func stringWithFormat(_ format: String) -> String {
-        
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = format
         return dateFormatter.string(from: self)
@@ -92,40 +69,12 @@ public extension Date {
     private static let cal = Calendar(identifier: .gregorian)
     
     public func isEqualDayTo(_ date: Date) -> Bool {
-        
         return self.withoutTimeComponent == date.withoutTimeComponent
     }
     
     public var withoutTimeComponent: Date {
-        
         return Date.cal.date(bySetting: .minute, value: 0, of:
                Date.cal.date(bySetting: .hour, value: 0, of: self)!)!
-    }
-    
-    public var withoutDayComponent: Date {
-        return Date.cal.date(bySetting: .day,    value: 0, of:
-               Date.cal.date(bySetting: .minute, value: 0, of:
-               Date.cal.date(bySetting: .hour,   value: 0, of: self)!)!)!
-    }
-    
-    public var timeString: String {
-        
-        return hourMinTimeFormatter.string(from: self)
-    }
-    
-    public var dateString: String {
-        
-        return dateFormatter.string(from: self)
-    }
-    
-    public static var currentDateTimeString: String {
-        
-        return dateTimeFormatter.string(from: Date())
-    }
-    
-    public static var currentDateString: String {
-        
-        return dateFormatter.string(from: Date())
     }
 }
 
@@ -133,7 +82,7 @@ public extension Int {
     
     public var hoursMinuteString: String {
         let date = Date(timeIntervalSinceReferenceDate: TimeInterval(self))
-        return minSecTimeFormatter.string(from: date)
+        return date.stringWithFormat("HH:mm")
     }
     
     public var timeComponentString: String {
